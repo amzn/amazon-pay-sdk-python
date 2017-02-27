34cc6fe4f90f8cb356c3895b171d271b5baaa214
Dan Schroff <schroffd@amazon.com>
2017-02-27 23:36:30 +0000

    Fix config

--- setup.py
+++ setup.py
@@ -1,62 +1,29 @@
-# For more details on how to operate this file, check
-# https://w.amazon.com/index.php/Python/Brazil
-import re
-from glob import glob
-from setuptools import setup, find_packages
-from brazilpython.setup_utilities import find_data_files, add_command_by_name
+from setuptools import setup
+import amazon_pay.version as pwa_version
 
-# if you have any python modules (.py files instead of dir/__init__.py
-# packages) list them here:
-py_modules = []
-
-# Add data files from the configuration/ directory, if it exists. By
-# default this is equivalent to has_configuration = true in your package
-# Config
-data_files = find_data_files('ApolloCmd', '*') + \
-             find_data_files('brazil-config', '*') + \
-             find_data_files('monitoring', '*') + \
-             find_data_files('static-content', '*') + \
-             find_data_files('sbin', '*')
-
-
-# declare your scripts:
-# scripts in bin/ with a shebang containing python will be
-# recognized automatically
-scripts = []
-for fname in glob('bin/*'):
-    with open(fname, 'r') as fh:
-        if re.search(r'^#!.*python', fh.readline()):
-            scripts.append(fname)
-
-
-# add custom commands here:
-cmdclass = {}
-
-# you should not need to modify anything below, unless you need to build
-# C extensions
-
-args = dict(
-    name='Amazon-Pay-SDK-python',
-    version='1.0',
-    # declare your packages
-    packages=find_packages(exclude=("test",)),
-    # declare your scripts
-    scripts=scripts,
-    # declare custom commads
-    cmdclass=cmdclass,
-    data_files=data_files,
-    options={
-        # make sure the right shebang is set for the scripts
-        'build_scripts': {
-            'executable': '/apollo/sbin/envroot "$ENVROOT/bin/python3.4"',
-            },
-        },
-    )
-
-add_command_by_name('flake8', args)
-add_command_by_name('build_sphinx', args)
-
-if py_modules:
-    args['py_modules'] = py_modules
-
-setup(**args)
+setup(
+    name='amazon_pay',
+    packages=['amazon_pay'],
+    version=pwa_version.versions['application_version'],
+    description='Amazon Pay Python SDK',
+    url='https://github.com/amzn/amazon-pay-sdk-python',
+    download_url='https://github.com/amzn/amazon-pay-sdk-python/tarball/{}'.format(
+        pwa_version.versions['application_version']),
+    author='EPS-DSE',
+    author_email='amazon-pay-sdk@amazon.com',
+    license='Apache License version 2.0, January 2004',
+    install_requires=['pyOpenSSL >= 0.11',
+                      'requests >= 2.6.0'],
+    keywords=['Amazon', 'Payments', 'Login', 'Python', 'API', 'SDK'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules']
+)
